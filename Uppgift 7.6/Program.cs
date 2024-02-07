430f4bb88afa44638dbff896490c81b0e332fba0
Dijar06 <dize06001@utb.vaxjo.se>
2024-02-07 09:38:11 +0100

    Add project files.

---
+++ Uppgift 7.6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in namn på matvaror:");
+            string matvaror = Console.ReadLine().ToLower();
+            string[] deladvaror = matvaror.Split(' ');
+
+            Console.WriteLine("Skriv in matvarornas priser:");
+            string priser = Console.ReadLine();
+            string[] deladpriser = priser.Split(' ');
+
+            Dictionary<string, int> handlingslista = new Dictionary<string, int>();
+
+            for (int i = 0; i < deladvaror.Length; i++)
+            {
+                handlingslista[deladvaror[i]] = int.Parse(deladpriser[i]);
+            }
+
+            Console.WriteLine("Skriv in din handlingslista");
+            string svar = Console.ReadLine().ToLower();
+            string[] deladsvar = svar.Split(' ');
+
+            int sum = 0;
+            for (int i = 0; i < deladsvar.Length; i++)
+            {
+                sum += handlingslista[deladsvar[i]];
+            }
+
+            Console.WriteLine("Priset för handlingslistan är " + sum);
+        }
+    }
+}
